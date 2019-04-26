33853866a1cb9c73244462a21134ee190d61d290
SHejder <syhanov.m@yandex.ru>
2019-04-26 13:04:17 +0300

    add

--- Google_test.py
+++ Google_test.py
@@ -2,59 +2,96 @@ import requests
 import functions as func
 import time
 import re
+import sys
+import design
+from PyQt5 import QtWidgets
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
-cookies = dict(SID='UQfgNZmamqHhoMVyF5m7ZbKxtBXR13_OIN3wA0FPoH_M4iCgbFj_TyzhQJCtx_d39x2W5g.')
-response = requests.get('https://www.google.com/search?num=100&q=site:' + input('Insert domain (without http(s)://):'),
-                        headers=headers,
-                        cookies=cookies
-                        )
+class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
-if response.status_code == 200:
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.starButton.clicked.connect(self.start)
 
-    links = []
 
-    print('Parsing the links...')
-    soup = BeautifulSoup(response.content, "html.parser")
-    items = soup.find_all('div', {'class': 'r'})
-    navigation = soup.find('div', {'id': 'navcnt'}).find_all('a', {'class': 'fl'})
 
-    # total = soup.find('div', {'id': 'resultStats'}).text
-    # total = int(re.sub('\\D', '', re.sub('\\(.*\\)', '', total)))
+    def extractor(self, items):
+        links = []
+        for item in items:
+            link = item.find('a').get('href')
+            link = re.sub('&sa=U.*', '', re.sub('/url\\?q=', '', link))
+            links.append(link)
+        return links
 
-    links += func.extractor(items)
+    def start(self):
+        text = self.lineEdit.text()
+        if text != '':
 
-    # if total > 100:
-    for nav in navigation:
-        href = nav.get('href')
-        response = requests.get('https://www.google.com' + href, headers=headers, cookies=cookies)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, "html.parser")
-            items = soup.find_all('div', {'class': 'r'})
-            links += func.extractor(items)
-            time.sleep(5)
-        else:
-            print('eCaptcha found in nav links')
-            raise SystemExit(1)
-    print('Found: ' + str(len(links)) + ' links')
+            self.lineEdit.setDisabled(True)
 
-else:
-    print('Bad URL or reCaptcha found! Try later')
-    raise SystemExit(1)
+            headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0'}
+            cookies = dict(SID='UQfgNZmamqHhoMVyF5m7ZbKxtBXR13_OIN3wA0FPoH_M4iCgbFj_TyzhQJCtx_d39x2W5g.')
+            response = requests.get('https://www.google.com/search?num=100&q=site:' + text,
+                                    headers=headers,
+                                    cookies=cookies
+                                    )
 
-print('Checking...')
+            if response.status_code == 200:
 
-i = 0
-f = open('results-' + str(datetime.today().strftime("%Y-%m-%d_%H-%M-%S")) + '.txt', 'w')
-for item in links:
-    response = requests.get(item)
-    if response.status_code != 200:
-        # print(item + ' response code not 200!')
-        f.write(item + ' response code not 200!' + '\n')
-        i += 1
-f.close()
+                links = []
 
-print('Check was complete, found ' + str(i) + ' bad links')
+                self.listWidget.addItem('Parsing the links...')
+                soup = BeautifulSoup(response.content, "html.parser")
+                items = soup.find_all('div', {'class': 'r'})
+                navigation = soup.find('div', {'id': 'navcnt'}).find_all('a', {'class': 'fl'})
+
+                # total = soup.find('div', {'id': 'resultStats'}).text
+                # total = int(re.sub('\\D', '', re.sub('\\(.*\\)', '', total)))
+
+                links += self.extractor(items)
+
+                # if total > 100:
+                for nav in navigation:
+                    href = nav.get('href')
+                    response = requests.get('https://www.google.com' + href, headers=headers, cookies=cookies)
+                    if response.status_code == 200:
+                        soup = BeautifulSoup(response.content, "html.parser")
+                        items = soup.find_all('div', {'class': 'r'})
+                        links += self.extractor(items)
+                        time.sleep(5)
+                    else:
+                        print('eCaptcha found in nav links')
+                        raise SystemExit(1)
+                self.listWidget.addItem('Found: ' + str(len(links)) + ' links')
+
+            else:
+                self.listWidget.addItem('Bad URL or reCaptcha found! Try later')
+                raise SystemExit(1)
+
+            self.listWidget.addItem('Checking...')
+
+            i = 0
+            f = open('results-' + str(datetime.today().strftime("%Y-%m-%d_%H-%M-%S")) + '.txt', 'w')
+            for item in links:
+                response = requests.get(item)
+                if response.status_code != 200:
+                    # print(item + ' response code not 200!')
+                    f.write(item + ' response code not 200!' + '\n')
+                    i += 1
+            f.close()
+
+            self.listWidget.addItem('Check was complete, found ' + str(i) + ' bad links')
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
---
+++ design.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'design.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(523, 523)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout.addWidget(self.lineEdit)
+        self.starButton = QtWidgets.QPushButton(self.centralwidget)
+        self.starButton.setObjectName("starButton")
+        self.horizontalLayout.addWidget(self.starButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setEnabled(False)
+        self.progressBar.setMouseTracking(False)
+        self.progressBar.setProperty("value", 24)
+        self.progressBar.setObjectName("progressBar")
+        self.verticalLayout.addWidget(self.progressBar)
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setObjectName("listWidget")
+        self.verticalLayout.addWidget(self.listWidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Insert a domain:"))
+        self.starButton.setText(_translate("MainWindow", "Start"))
+
